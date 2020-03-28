994770a21a9b672029d2b1474c50feea30e803c3
Daniel <Scriptor.Deo@gmail.com>
2020-03-28 11:33:33 -0400

    First Automated Post

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ var bot = new Twit({
     consumer_key: process.env.MICAHBOT_CONSUMER_KEY,
     consumer_secret: process.env.MICAHBOT_CONSUMER_SECRET,
     access_token: process.env.MICAHBOT_ACCESS_TOKEN,
-    access_token_secret: process.env.MICAHBOT_ACCESS_TOKEN,
+    access_token_secret: process.env.MICAHBOT_ACCESS_TOKEN_SECRET,
     timeout_ms: 60*1000
 });
 
+bot.post('statuses/update', {status: 'hello world!'}, function(err, data, response){
+    if (err){
+        console.log(err);
+    } else {
+        console.log(data.text + ' was tweeted.')
+    }
+});
